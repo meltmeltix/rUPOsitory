c7e499561cb15650f09ac9a423a83a240e1eb974
Alessio <alessioc.developer@gmail.com>
2024-05-30 12:22:28 +0200

    Added integer arrays for getMooreMaxJobs

--- Algoritmi 2/Graphs/src/test/upo/algotechniques/GreedyTest.java
+++ Algoritmi 2/Graphs/src/test/upo/algotechniques/GreedyTest.java
@@ -6,8 +6,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GreedyTest {
     
+    // https://steemit.com/ita/@thegreenfox/tecniche-di-scheduling-algoritmo-di-moore
     @Test
     void getMooreMaxJobs() {
+        int[] duration = { 8, 7, 3, 4, 11 };
+        int[] deadline = { 11, 10, 17, 14, 22 };
+    
     }
     
 }
